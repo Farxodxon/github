e2949162d587b95de5da1da2669c85a8f3bfe108
Farxod <doxtuit99@gmail.com>
2022-07-15 22:20:19 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:chat_flutter/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized(); // Add this
+  await Firebase.initializeApp(name: "Chat App",
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Chat',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home:  ChatScreen(),
+          );
+  }
+}
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream:  FirebaseFirestore.instance
+            .collection("chats/l4BzlCOguaOcF1Qs6fg7/messages")
+            .snapshots(),
+        builder: (ctx, streamSnapshot){
+          return ListView.builder(
+            itemCount: 10,
+            itemBuilder: (ctx, index) => Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('This Works!'),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          final doc = FirebaseFirestore.instance
+              .collection("chats/l4BzlCOguaOcF1Qs6fg7/messages")
+              .snapshots();
+          doc.listen((data) {
+            data.docs.forEach((document) {
+              print(document['text']);
+            });
+          });
+        },
+      ),
+    );
+  }
+}
